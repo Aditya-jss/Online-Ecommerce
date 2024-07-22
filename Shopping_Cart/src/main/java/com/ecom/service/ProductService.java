0fb3f4d4ec986b5bc389d6447b0d44ab8c351df5
Aditya-jss <user@10.1.10.211>
2024-07-21 19:40:13 -0700

    Backend update

---
+++ Shopping_Cart/src/main/java/com/ecom/service/ProductService.java
@@ -0,0 +1,15 @@
+//package com.ecom.service;
+//
+//import java.util.List;
+//
+//import org.springframework.web.multipart.MultipartFile;
+//
+//import com.ecom.model.Product;
+//
+//public interface ProductService {
+//
+//	public Product saveProduct(Product product);
+//  public List <Product> getAllProducts();
+//  public Boolean deleteProduct(Integer id);
+//}
+//
